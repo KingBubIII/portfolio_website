784c2f42001b41b2a78effa00f9d1ad25bc24126
Caleb Richardson <richardsoncaleb60@gmail.com>
2024-06-26 10:52:13 -0600

    touched up navbar visually and added links to connect the website and social media

--- devsite/me_info/views.py
+++ devsite/me_info/views.py
@@ -6,7 +6,9 @@ from os import listdir
 def intro(request):
     images_dir = listdir(str(BASE_DIR)+"\\devsite\\me_info\\static\\img\\")
     images_paths = [MEDIA_URL+img for img in images_dir]
-    # inital_image_path = images_paths.pop(0)
+
+    navbar_links = {"about_me":"intro", "projects": "projects", "work_history": "work-history"}
+
     context = {"image_paths": images_paths}
     return render(request, "me_info\\intro.html", context)
 
