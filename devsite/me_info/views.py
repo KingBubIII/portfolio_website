152b2aeb55ce0d849b6b75fe3b36a391cfeb180b
Caleb Richardson <richardsoncaleb60@gmail.com>
2024-07-01 13:17:13 -0600

    updated navbar to show all projects under the dropdown menu. Also enables scrolling to that specific project when clicking the link

--- devsite/me_info/views.py
+++ devsite/me_info/views.py
@@ -8,13 +8,20 @@ def intro(request):
     intro_data = load(open(JSON_PATH + "about_me.json"))["intro"]
     intro_data["image_paths"] = [MEDIA_URL+img for img in intro_data["image_paths"]]
 
-    return render(request, "me_info\\intro.html", intro_data)
+    projects_data = load(open(JSON_PATH + "about_me.json"))["projects"]
+
+    context = {"intro_data": intro_data, "projects_data": projects_data}
+
+    return render(request, "me_info\\intro.html", context)
 
 def work_history(request):
     work_history_data = load(open(JSON_PATH + "about_me.json"))["work_history"]
     for job_index in range(len(work_history_data)):
         work_history_data[job_index]["company_image"] = MEDIA_URL+work_history_data[job_index]["company_image"]
-    context = {"work_history_data": work_history_data}
+
+    projects_data = load(open(JSON_PATH + "about_me.json"))["projects"]
+
+    context = {"work_history_data": work_history_data, "projects_data": projects_data}
     return render(request, "me_info\\work_history.html", context)
 
 def projects(request):
