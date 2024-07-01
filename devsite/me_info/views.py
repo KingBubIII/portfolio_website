45012f6a2f7eb4d58c0ac4fe4c5665589257767a
Caleb Richardson <richardsoncaleb60@gmail.com>
2024-06-30 21:49:22 -0600

    Updated intro url with data from json file

--- devsite/me_info/views.py
+++ devsite/me_info/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
-from settings import MEDIA_URL, BASE_DIR, JSON_PATH
+from settings import MEDIA_URL, BASE_DIR, JSON_PATH, STATIC_URL
 from os import listdir
 from json import load
 
 # Create your views here.
 def intro(request):
-    images_dir = listdir(str(BASE_DIR)+"\\devsite\\me_info\\static\\img\\")
-    images_paths = [MEDIA_URL+img for img in images_dir]
+    intro_data = load(open(JSON_PATH + "about_me.json"))["intro"]
+    intro_data["image_paths"] = [MEDIA_URL+img for img in intro_data["image_paths"]]
 
-    navbar_links = {"about_me":"intro", "projects": "projects", "work_history": "work-history"}
-
-    context = {"image_paths": images_paths}
-    return render(request, "me_info\\intro.html", context)
+    return render(request, "me_info\\intro.html", intro_data)
 
 def work_history(request):
     work_history_data = load(open(JSON_PATH + "about_me.json"))["work_history"]
