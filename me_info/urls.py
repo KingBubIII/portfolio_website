59f3e371005901d8fbede6af9fd1f728800527d7
Caleb Richardson <richardsoncaleb60@gmail.com>
2024-07-18 10:59:01 -0600

    editted a template to work for my purposes

--- me_info/urls.py
+++ me_info/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home),
     path("intro/", views.intro),
     path("work-history/", views.work_history),
     path("projects/", views.projects)
--- me_info/views.py
+++ me_info/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 from django.conf import settings
 from json import load
 
+def home(request):
+    info_dict = load(open(settings.JSON_PATH + "about_me.json"))
+    context = {}
+    print(info_dict)
+    context["intro"] = info_dict["intro"]
+    context["work_history"] = info_dict["work_history"]
+    context["me_images"] = info_dict["me_images"]
+    context["education"] = info_dict["education"]
+    context["self_learning"] = info_dict["self_learning"]
+    context["projects"] = info_dict["projects"]
+    context["all_skills"] = list(set([skill for project in context["projects"] for skill in project["skills"]]))
+    return render(request, 'home.html', context)
+
 # Create your views here.
 def intro(request):
     intro_data = load(open(settings.JSON_PATH + "about_me.json"))["intro"]
