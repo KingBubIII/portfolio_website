3fcfcce596db03514096f79b77b2cd74dd6739f7
Caleb Richardson <richardsoncaleb60@gmail.com>
2024-07-18 11:10:44 -0600

    removed old url paths that no longer work or are nessessary

--- me_info/urls.py
+++ me_info/urls.py
@@ -3,8 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="about-me-home"),
-    path("intro/", views.intro),
-    path("work-history/", views.work_history),
-    path("projects/", views.projects)
+    path("", views.home, name="about-me-home")
 ]
--- me_info/views.py
+++ me_info/views.py
@@ -13,29 +13,3 @@ def home(request):
     context["projects"] = info_dict["projects"]
     context["all_skills"] = list(set([skill for project in context["projects"] for skill in project["skills"]]))
     return render(request, 'home.html', context)
-
-# Create your views here.
-def intro(request):
-    intro_data = load(open(settings.JSON_PATH + "about_me.json"))["intro"]
-    intro_data["image_paths"] = [settings.MEDIA_URL + img for img in intro_data["image_paths"]]
-
-    projects_data = load(open(settings.JSON_PATH + "about_me.json"))["projects"]
-
-    context = {"intro_data": intro_data, "projects_data": projects_data}
-
-    return render(request, "intro.html", context)
-
-def work_history(request):
-    work_history_data = load(open(settings.JSON_PATH + "about_me.json"))["work_history"]
-    for job_index in range(len(work_history_data)):
-        work_history_data[job_index]["company_image"] = settings.MEDIA_URL + work_history_data[job_index]["company_image"]
-
-    projects_data = load(open(settings.JSON_PATH + "about_me.json"))["projects"]
-
-    context = {"work_history_data": work_history_data, "projects_data": projects_data}
-    return render(request, "work_history.html", context)
-
-def projects(request):
-    projects_data = load(open(settings.JSON_PATH + "about_me.json"))["projects"]
-    context = {"projects_data": projects_data}
-    return render(request, "projects.html", context)
